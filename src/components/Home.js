47a02dd5d8d34934ce6d23f5f3886f7d06ca0d93
Shane Saravia <shanesaravia@live.ca>
2020-03-17 23:46:21 -0400

    featured projects added to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Typography, Grid, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import projectsContent from './Projects/modules/projectsContent';
+import ProjectTile from './Projects/modules/ProjectTile';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   header: {
     fontFamily: 'Muli'
   },
@@ -11,12 +13,23 @@ const useStyles = makeStyles({
   },
   paper: {
     padding: 40
+  },
+  featured: {
+    fontFamily: 'Muli',
+    marginTop: 30,
+    marginBottom: 20
+  },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, 300px)',
+    gridGap: 20
   }
-})
+}))
+
 
 const Home = () => {
   const classes = useStyles();
-
+  
   return (
     <Paper className={classes.paper}>
     <Grid
@@ -33,6 +46,18 @@ const Home = () => {
       <Typography variant='caption' className={classes.subheader}>
         Specializing in full stack software development.
       </Typography>
+      <Typography className={classes.featured} variant='h5'>
+        Featured Projects
+      </Typography>
+      <Grid container justify='center' className={classes.container}>
+        {
+          projectsContent.map((project, idx) => (
+            <Grid item key={idx}>
+              <ProjectTile {...project} />
+            </Grid>
+          ))
+        }
+      </Grid>
     </Grid>
     </Paper>
   )

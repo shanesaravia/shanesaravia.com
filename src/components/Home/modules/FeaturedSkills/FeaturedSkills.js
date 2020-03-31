24cd8da68bdc2320055e393d3a75b5452bd2a2a4
Shane Saravia <shanesaravia@live.ca>
2020-03-31 15:15:15 -0400

    wip

--- src/components/Home/modules/FeaturedSkills/FeaturedSkills.js
+++ src/components/Home/modules/FeaturedSkills/FeaturedSkills.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Grid } from '@material-ui/core';
 import Skills from './modules/Skills';
+import TableImage from './modules/TableImage';
 
 const useStyles = makeStyles(theme => ({
   featured: {
     fontFamily: 'Muli',
     marginTop: theme.spacing(4),
     marginBottom: theme.spacing(2)
+  },
+  table: {
   }
 }))
 
@@ -20,12 +23,13 @@ const FeaturedSkills = () => {
       </Typography>
       <Grid container>
         <Grid container item xs={6} justify='center'>
-          <Skills body='body1' head='head3' face='face3' />
+          <Skills body='body1' head='head3' face='face3' img='python.png' shirtColor='#B22222' />
         </Grid>
         <Grid container item xs={6} justify='center'>
-          <Skills body='body2' head='head2' face='face2' />
+          <Skills body='body2' head='head2' face='face2' img='javascript.png' shirtColor='#191970' />
         </Grid>
       </Grid>
+      <TableImage className={classes.table} />
     </>
   )
 }
